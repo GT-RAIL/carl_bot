410c5ccd19addafb0007a6c26989e23038704c9e
Steven Kordell <spkordell@gmail.com>
2014-05-27 09:58:01 -0400

    Added fixes is response to code review: authorship credit, doxygen, and formatting.

--- carl_teleop/src/carl_joy_teleop.cpp
+++ carl_teleop/src/carl_joy_teleop.cpp
@@ -1,15 +1,18 @@
 /*!
-* \carl_joy_teleop.cpp
-* \brief Allows for control of CARL with a joystick.
-*
-* carl_joy_teleop creates a ROS node that allows the control of CARL with a joystick.
-* This node listens to a /joy topic and sends messages to the /cmd_vel topic.
-*
-* \author Steven Kordell, WPI - spkordell@wpi.edu
-* \date May 23, 2014
-*/
-
-
+ * \carl_joy_teleop.cpp
+ * \brief Allows for control of CARL with a joystick.
+ *
+ * carl_joy_teleop creates a ROS node that allows the control of CARL with a joystick.
+ * This node listens to a /joy topic and sends messages to the /cmd_vel topic.
+ *
+ *
+ * \author Russell Toris, WPI - rctoris@wpi.edu
+ * \date May 21, 2013
+ *
+ * \author Steven Kordell, WPI - spkordell@wpi.edu
+ * \date May 23, 2014
+ *
+ */
 
 #include <geometry_msgs/Twist.h>
 #include <ros/ros.h>
@@ -21,7 +24,8 @@ bool receivedmsg = false;
 
 using namespace std;
 
-carl_joy_teleop::carl_joy_teleop() {
+carl_joy_teleop::carl_joy_teleop()
+{
   // create the ROS topics
   cmd_vel = node.advertise < geometry_msgs::Twist > ("cmd_vel", 10);
   joy_sub = node.subscribe < sensor_msgs::Joy > ("joy", 10, &carl_joy_teleop::joy_cback, this);
@@ -32,7 +36,7 @@ carl_joy_teleop::carl_joy_teleop() {
     linear_throttle_factor = (float)temp;
   else
     linear_throttle_factor = 1.0;
-  if (node.getParam("/youbot_joy_teleop/angular_throttle_factor", temp))
+  if (node.getParam("/carl_joy_teleop/angular_throttle_factor", temp))
     angular_throttle_factor = (float)temp;
   else
     angular_throttle_factor = 1.0;
@@ -40,9 +44,10 @@ carl_joy_teleop::carl_joy_teleop() {
   ROS_INFO("Carl Joystick Teleop Started");
 }
 
-
-void carl_joy_teleop::joy_cback(const sensor_msgs::Joy::ConstPtr& joy) {
-  if(!receivedmsg) {
+void carl_joy_teleop::joy_cback(const sensor_msgs::Joy::ConstPtr& joy)
+{
+  if (!receivedmsg)
+  {
     receivedmsg = true;
   }
   T = ros::Time::now();
@@ -55,33 +60,40 @@ void carl_joy_teleop::joy_cback(const sensor_msgs::Joy::ConstPtr& joy) {
   twist.angular.x = 0;
   twist.angular.y = 0;
 
-  if (joy->buttons.at(4) == 1) {
-	  // left joystick controls the linear and angular movement
-	  twist.linear.x = joy->axes.at(1)*MAX_TRANS_VEL*linear_throttle_factor;
-	  twist.angular.z = joy->axes.at(0)*MAX_ANG_VEL*angular_throttle_factor;
-  } else {
-	  twist.linear.x = 0;
-	  twist.angular.z = 0;
+  if (joy->buttons.at(4) == 1)
+  {
+    // left joystick controls the linear and angular movement
+    twist.linear.x = joy->axes.at(1) * MAX_TRANS_VEL * linear_throttle_factor;
+    twist.angular.z = joy->axes.at(0) * MAX_ANG_VEL * angular_throttle_factor;
+  }
+  else
+  {
+    twist.linear.x = 0;
+    twist.angular.z = 0;
   }
 
   //boost button
-  if (joy->buttons.at(5) == 1) {
-	  twist.linear.x*=2;
-	  twist.angular.z*=2;
+  if (joy->buttons.at(5) == 1)
+  {
+    twist.linear.x *= 2;
+    twist.angular.z *= 2;
   }
 
   // send the twist command
   cmd_vel.publish(twist);
 }
 
-void carl_joy_teleop::joy_check() {
-    if( (receivedmsg) && ( (ros::Time::now().toSec() - T.toSec() ) > .15) ) {
-      geometry_msgs::Twist zero;
-      cmd_vel.publish(zero);
-    }
+void carl_joy_teleop::joy_check()
+{
+  if ((receivedmsg) && ((ros::Time::now().toSec() - T.toSec()) > .15))
+  {
+    geometry_msgs::Twist zero;
+    cmd_vel.publish(zero);
+  }
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   // initialize ROS and the node
   ros::init(argc, argv, "carl_joy_teleop");
 
@@ -90,9 +102,9 @@ int main(int argc, char **argv) {
 
   // continue until a ctrl-c has occurred
   /*while(ros::ok()) {
-	  controller.joy_check();
+   controller.joy_check();
 
-	  ros::spinOnce();
-  }*/
+   ros::spinOnce();
+   }*/
   ros::spin();
 }
--- carl_teleop/src/carl_key_teleop.cpp
+++ carl_teleop/src/carl_key_teleop.cpp
@@ -1,13 +1,42 @@
+/*
+ * Copyright (c) 2011, Willow Garage, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * * Neither the name of the Willow Garage, Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 /*!
-* \carl_key_teleop.cpp
-* \brief Allows for control of CARL with a keyboard.
-*
-* carl_joy_teleop creates a ROS node that allows the control of CARL with a keyboard.
-* This node listens to a /joy topic and sends messages to the /cmd_vel topic.
-*
-* \author Steven Kordell, WPI - spkordell@wpi.edu
-* \date May 23, 2014
-*/
+ * \carl_key_teleop.cpp
+ * \brief Allows for control of CARL with a keyboard.
+ *
+ * carl_joy_teleop creates a ROS node that allows the control of CARL with a keyboard.
+ * This node listens to a /joy topic and sends messages to the /cmd_vel topic.
+ *
+ * \author Steven Kordell, WPI - spkordell@wpi.edu
+ * \date May 23, 2014
+ */
 
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
@@ -24,37 +53,34 @@
 #define KEYCODE_D 0x42
 #define KEYCODE_Q 0x71
 
-carl_key_teleop::carl_key_teleop():
-  ph_("~"),
-  linear_(0),
-  angular_(0),
-  l_scale_(1.0),
-  a_scale_(1.0)
-  {
-	ph_.param("scale_angular", a_scale_, a_scale_);
-	ph_.param("scale_linear", l_scale_, l_scale_);
+carl_key_teleop::carl_key_teleop() :
+    ph_("~"), linear_(0), angular_(0), l_scale_(1.0), a_scale_(1.0)
+{
+  ph_.param("scale_angular", a_scale_, a_scale_);
+  ph_.param("scale_linear", l_scale_, l_scale_);
 
-	vel_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
-  }
+  vel_pub_ = nh_.advertise < geometry_msgs::Twist > ("cmd_vel", 1);
+}
 
 int kfd = 0;
 struct termios cooked, raw;
 
-
-void carl_key_teleop::watchdog() {
+void carl_key_teleop::watchdog()
+{
   boost::mutex::scoped_lock lock(publish_mutex_);
-  if ((ros::Time::now() > last_publish_ + ros::Duration(0.15)) &&
-      (ros::Time::now() > first_publish_ + ros::Duration(0.50)))
+  if ((ros::Time::now() > last_publish_ + ros::Duration(0.15))
+      && (ros::Time::now() > first_publish_ + ros::Duration(0.50)))
     publish(0, 0);
 }
 
-void carl_key_teleop::keyLoop() {
+void carl_key_teleop::keyLoop()
+{
   char c;
 
   // get the console in raw mode
   tcgetattr(kfd, &cooked);
   memcpy(&raw, &cooked, sizeof(struct termios));
-  raw.c_lflag &=~ (ICANON | ECHO);
+  raw.c_lflag &= ~(ICANON | ECHO);
   // Setting a new line, then end of file
   raw.c_cc[VEOL] = 1;
   raw.c_cc[VEOF] = 2;
@@ -64,19 +90,20 @@ void carl_key_teleop::keyLoop() {
   puts("---------------------------");
   puts("Use arrow keys to move carl.");
 
-
-  while (ros::ok()) {
+  while (ros::ok())
+  {
     // get the next event from the keyboard
-    if(read(kfd, &c, 1) < 0)
+    if (read(kfd, &c, 1) < 0)
     {
       perror("read():");
       exit(-1);
     }
 
-    linear_=angular_=0;
+    linear_ = angular_ = 0;
     ROS_DEBUG("value: 0x%02X\n", c);
 
-    switch(c) {
+    switch (c)
+    {
       case KEYCODE_L:
         ROS_DEBUG("LEFT");
         angular_ = 1.0;
@@ -95,7 +122,8 @@ void carl_key_teleop::keyLoop() {
         break;
     }
     boost::mutex::scoped_lock lock(publish_mutex_);
-    if (ros::Time::now() > last_publish_ + ros::Duration(1.0)) {
+    if (ros::Time::now() > last_publish_ + ros::Duration(1.0))
+    {
       first_publish_ = ros::Time::now();
     }
     last_publish_ = ros::Time::now();
@@ -105,27 +133,30 @@ void carl_key_teleop::keyLoop() {
   return;
 }
 
-void carl_key_teleop::publish(double angular, double linear) {
-    geometry_msgs::Twist vel;
-    vel.angular.z = a_scale_*angular;
-    vel.linear.x = l_scale_*linear;
+void carl_key_teleop::publish(double angular, double linear)
+{
+  geometry_msgs::Twist vel;
+  vel.angular.z = a_scale_ * angular;
+  vel.linear.x = l_scale_ * linear;
 
-    vel_pub_.publish(vel);
-    return;
+  vel_pub_.publish(vel);
+  return;
 }
 
-void quit(int sig) {
+void quit(int sig)
+{
   tcsetattr(kfd, TCSANOW, &cooked);
   ros::shutdown();
   exit(0);
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
   ros::init(argc, argv, "carl_key_teleop");
   carl_key_teleop key_controller;
   ros::NodeHandle n;
 
-  signal(SIGINT,quit);
+  signal(SIGINT, quit);
 
   boost::thread my_thread(boost::bind(&carl_key_teleop::keyLoop, &key_controller));
 
@@ -136,5 +167,5 @@ int main(int argc, char** argv) {
   my_thread.interrupt();
   my_thread.join();
 
-  return(0);
+  return (0);
 }
