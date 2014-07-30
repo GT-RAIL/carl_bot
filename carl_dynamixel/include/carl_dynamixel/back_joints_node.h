18848e7882c092664c6149a985c9fb5bbc46b593
Chris Dunkers <cmdunkers@gmail.com>
2014-07-30 09:41:38 -0400

    updated carl_dynamixel to publish joint states and added a yaml to define the joints

---
+++ carl_dynamixel/include/carl_dynamixel/back_joints_node.h
@@ -0,0 +1,45 @@
+/*
+ * \back_joints_node.h
+ * \allows the stopping of the segway base without losing power to peripherals
+ *
+ * carl_estop_node creates a ROS node that allows the stopping of the robot after a specified amount of time.
+ * This node listens to a /carl_estop topic
+ * and sends messages to the /move_base actionlib to stop the current execution of a goal.
+ *
+ * \author Russell Toris, WPI - rctoris@wpi.edu *
+ * \author Chris Dunkers, WPI - cmdunkers@wpi.edu
+ * \date July 24, 2014
+ */
+
+#ifndef BACK_JOINTS_NODE_H_
+#define BACK_JOINTS_NODE_H_
+
+#include "ros/ros.h"
+#include "sensor_msgs/JointState.h"
+#include "dynamixel_msgs/MotorStateList.h"
+#include "dynamixel_msgs/MotorState.h"
+
+class back_joints
+{
+  public:
+    back_joints();
+    /*
+     * estop function to check the time difference to see if any goal position in move_base should be cancelled
+     *
+     * \param msg the empty message
+     */
+    void joints(const dynamixel_msgs::MotorStateList::ConstPtr& state);
+
+  private:
+	//main node handle
+	ros::NodeHandle node;
+
+    // the ros subscriber and publisher
+    ros::Subscriber asus_sub;
+    ros::Publisher joint_states_pub;
+
+    //map to store the ids and the link names of each servo connected
+    std::map<int,std::string> back_servos;
+};
+
+#endif
---
+++ carl_dynamixel/src/back_joint_node.cpp
@@ -0,0 +1,67 @@
+#include "carl_dynamixel/back_joints_node.h"
+
+/*
+ * Constructor to inialize the node
+ */
+back_joints::back_joints() 
+{
+	std::string servo_name;
+	int num, id;
+	
+	ros::NodeHandle private_node_handle_("~");
+	node.getParam("back_servos/num_servos", num);
+	for(int s = 1; s <= num; s++){
+		std::stringstream ss;
+		ss<<s;
+		std::string servo_num = ss.str();
+		std::string param_id ("back_servos/" + servo_num + "/id");
+		std::string param_name ("back_servos/" + servo_num + "/link_name");
+		node.getParam(param_id,id);
+		node.getParam(param_name,servo_name);
+		back_servos[id] = servo_name;
+	}
+	
+	//setup the subscriber
+    asus_sub = node.subscribe("motor_states/back_port", 1000, &back_joints::joints, this);
+    
+    //setup the publisher
+	joint_states_pub = node.advertise<sensor_msgs::JointState>("dynamixel_back", 1);
+    
+    
+}
+
+void back_joints::joints(const dynamixel_msgs::MotorStateList::ConstPtr& state){
+	sensor_msgs::JointState back_states;
+	for(int s = 0; s < state->motor_states.size(); s++){
+		dynamixel_msgs::MotorState servo = state->motor_states[s];
+		std:string name = back_servos[servo.id];
+		if(name.trim().size() == 0){
+			name = "UNDEFINED";
+		}
+		back_states.name.push_back(name);
+		back_states.position.push_back(servo.position);
+	}
+	joint_states_pub.publish(back_states);
+}
+
+/*
+ * main function
+ */
+int main(int argc, char **argv)
+{
+	//initialize the node
+	ros::init(argc, argv, "back_dynamixel");
+
+	// initialize the joystick controller
+	back_joints back;
+
+	//main loop
+	ros::Rate loop_rate(30);
+	while (ros::ok()) 
+	{
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+
+	return EXIT_SUCCESS;
+}
