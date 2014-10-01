5a2339abafaec771aa12fce33e714d73a1afe408
Russell Toris <rctoris@wpi.edu>
2014-10-01 11:17:12 -0400

    Revert "Changes to base teleop to increase safety"
    This reverts commit ff0796d01abd7b8640db59a5e7789940692b9b4c.

--- carl_teleop/include/carl_teleop/carl_joy_teleop.h
+++ carl_teleop/include/carl_teleop/carl_joy_teleop.h
@@ -9,8 +9,7 @@
  * \author David Kent, WPI - davidkent@wpi.edu
  * \author Russell Toris, WPI - rctoris@wpi.edu
  * \author Steven Kordell, WPI - spkordell@wpi.edu
- * \author Brian Hetherman, WPI - bhetherman@wpi.edu
- * \date August 8, 2014
+ * \date July 24, 2014
  */
 
 #ifndef CARL_JOY_TELEOP_H_
@@ -18,14 +17,8 @@
 
 #include <ros/ros.h>
 #include <sensor_msgs/Joy.h>
-#include <sensor_msgs/LaserScan.h>
 #include <std_msgs/Float64.h>
 #include <wpi_jaco_msgs/CartesianCommand.h>
-#include <geometry_msgs/PointStamped.h>
-#include <geometry_msgs/PoseWithCovarianceStamped.h>
-#include <tf/transform_listener.h>
-#include <nav_msgs/OccupancyGrid.h>
-#include <math.h>
 
 //Control modes
 #define ARM_CONTROL 0 
@@ -81,22 +74,6 @@
 #define NON_BOOST_THROTTLE 0.8
 
 /*!
- * \def START_SAFETY_THROTTLE_DIST
- *
- * The dist to start throttling linear vel for safety.
- */
-#define START_SAFETY_THROTTLE_DIST 1.25
-
-/*!
- * \def MIN_SAFE_DIST
- *
- * The dist to start throttling linear vel for safety.
- */
-#define MIN_SAFE_DIST 0.55
-
-
-
-/*!
  * \class carl_joy_teleop
  * \brief Allows for control of CARL with a joystick.
  *
@@ -125,49 +102,22 @@ private:
    */
   void joy_cback(const sensor_msgs::Joy::ConstPtr& joy);
 
-  /*!
-   * Scan topic callback function.
-   *
-   * \param scan - the message for the scan topic
-   */
-  void scan_cback(const sensor_msgs::LaserScan::ConstPtr& scan);
-
-  /*!
-   * map topic callback function.
-   *
-   * \param map - the message for the map topic
-   */
-  void map_cback(const nav_msgs::OccupancyGrid::ConstPtr& map);
-
-  /*!
-   * amcl_pose topic callback function.
-   *
-   * \param pose - the message for the amcl_pose topic
-   */
-  void amcl_pose_cback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& pose);
-
   void displayHelp(int menuNumber);
 
   ros::NodeHandle node; /*!< a handle for this ROS node */
-  tf::TransformListener* pListener;
 
   ros::Publisher cmd_vel; /*!< the base cmd_vel topic */
   ros::Publisher cartesian_cmd; /*!< cartesian arm command topic */
   ros::Publisher asus_servo_tilt_cmd; /*< velocity command to tilt the asus servo */
   ros::Publisher creative_servo_pan_cmd; /*< velocity command to pan the creative servo */
   ros::Subscriber joy_sub; /*!< the joy topic */
-  ros::Subscriber scan_sub; /*!< the scan topic */
-  ros::Subscriber map_sub; /*!< the map topic */
-  ros::Subscriber amcl_pose_sub; /*!< the amcl_pose topic */
 
   geometry_msgs::Twist twist; /*!< base movement command */
   wpi_jaco_msgs::CartesianCommand fingerCmd; /*!< angular movement command */
   wpi_jaco_msgs::CartesianCommand cartesianCmd; /*!< cartesian movement command */
-  nav_msgs::OccupancyGrid savedMap; /*!< map of allowed speeds */
 
   int mode; /*!< the control mode */
   int controllerType; /*!< the type of joystick controller */
-  int cmd_vel_safety; /*!< the type of joystick controller */
   double linear_throttle_factor_base; /*!< factor for reducing the base maximum linear speed */
   double angular_throttle_factor_base; /*!< factor for reducing the base maximum angular speed */
   double linear_throttle_factor_arm; /*!< factor for reducing the arm linear speed */
@@ -181,9 +131,6 @@ private:
   bool EStopEnabled; /*!< software emergency stop for the arm*/
   bool helpDisplayed; /*!< flag so help is not repeatedly displayed*/
   bool deadman; /*!< save state of deadman switch */
-
-  double forward_throttle_safety_factor_base; /*!< factor for reducing the base maximum positive linear speed from laser scan*/
-  double reverse_throttle_safety_factor_base; /*!< factor for reducing the base maximum negetive linear speed from map*/
 };
 
 /*!
--- carl_teleop/src/carl_joy_teleop.cpp
+++ carl_teleop/src/carl_joy_teleop.cpp
@@ -9,12 +9,11 @@
  * \author David Kent, WPI - davidkent@wpi.edu
  * \author Russell Toris, WPI - rctoris@wpi.edu
  * \author Steven Kordell, WPI - spkordell@wpi.edu
- * \author Brian Hetherman, WPI - bhetherman@wpi.edu
- * \date August 8, 2014
+ * \date July 24, 2014
  */
 
 #include <carl_teleop/carl_joy_teleop.h>
-#include <unistd.h>
+
 using namespace std;
 
 carl_joy_teleop::carl_joy_teleop()
@@ -22,17 +21,12 @@ carl_joy_teleop::carl_joy_teleop()
   // a private handle for this ROS node (allows retrieval of relative parameters)
   ros::NodeHandle private_nh("~");
 
-  
-
   // create the ROS topics
   cmd_vel = node.advertise<geometry_msgs::Twist>("cmd_vel", 10);
   cartesian_cmd = node.advertise<wpi_jaco_msgs::CartesianCommand>("jaco_arm/cartesian_cmd", 10);
   asus_servo_tilt_cmd = node.advertise<std_msgs::Float64>("asus_controller/tilt", 10);
   creative_servo_pan_cmd = node.advertise<std_msgs::Float64>("creative_controller/pan", 10);
   joy_sub = node.subscribe<sensor_msgs::Joy>("joy", 10, &carl_joy_teleop::joy_cback, this);
-  scan_sub = node.subscribe<sensor_msgs::LaserScan>("scan", 10, &carl_joy_teleop::scan_cback, this);
-  map_sub = node.subscribe<nav_msgs::OccupancyGrid>("/map", 10, &carl_joy_teleop::map_cback, this);
-  amcl_pose_sub = node.subscribe<geometry_msgs::PoseWithCovarianceStamped>("/amcl_pose", 10, &carl_joy_teleop::amcl_pose_cback, this);
 
   // read in throttle values
   private_nh.param<double>("linear_throttle_factor_base", linear_throttle_factor_base, 1.0);
@@ -48,10 +42,6 @@ carl_joy_teleop::carl_joy_teleop()
     controllerType = ANALOG;
 
   // initialize everything
-  pListener = new (tf::TransformListener);
-
-  forward_throttle_safety_factor_base = 0.0;
-  reverse_throttle_safety_factor_base = 0.0;
   deadman = false;
   stopMessageSentArm = true;
   stopMessageSentFinger = true;
@@ -68,10 +58,10 @@ carl_joy_teleop::carl_joy_teleop()
   cartesianCmd.fingerCommand = false;
   cartesianCmd.repeat = true;
 
-  ROS_INFO("CARL Joystick Teleop Started (Brian)");
+  ROS_INFO("CARL Joystick Teleop Started");
 
   puts(" ---------------------------------------------------");
-  puts("| CARL Joystick Teleop Help  (Brian)                |");
+  puts("| CARL Joystick Teleop Help                         |");
   puts("|---------------------------------------------------|*");
   if (mode == BASE_CONTROL)
     puts("| Current Mode: Base Control                        |*");
@@ -208,10 +198,6 @@ void carl_joy_teleop::joy_cback(const sensor_msgs::Joy::ConstPtr& joy)
       {
         // left joystick controls the linear and angular movement
         twist.linear.x = joy->axes.at(1) * MAX_TRANS_VEL_BASE * linear_throttle_factor_base;
-        if(twist.linear.x > 0)
-	  twist.linear.x *= forward_throttle_safety_factor_base;
-        else if(twist.linear.x < 0)
-          twist.linear.x *= reverse_throttle_safety_factor_base;
         if (controllerType == DIGITAL)
           twist.angular.z = joy->axes.at(2) * MAX_ANG_VEL_BASE * angular_throttle_factor_base;
         else
@@ -453,132 +439,6 @@ void carl_joy_teleop::joy_cback(const sensor_msgs::Joy::ConstPtr& joy)
       
     break;
   }
-}
-
-void carl_joy_teleop::scan_cback(const sensor_msgs::LaserScan::ConstPtr& ptr)
-{
-  static int index = 0;
-  static double closestDists[2];
-  geometry_msgs::PointStamped laser_point;
-  geometry_msgs::PointStamped base_point;
-
-  sensor_msgs::LaserScan scan = *ptr;
-
-  laser_point.header.frame_id = "hokuyo_link";
-  laser_point.header.stamp = scan.header.stamp;
-  laser_point.point.z = 0.0;
-
-  int i = 0;
-  double currentAngle;
-  double closestDist = scan.range_max;
-
-  for(currentAngle = scan.angle_min; currentAngle < scan.angle_max; currentAngle+=scan.angle_increment){
-    //if the point is not within range
-    if(scan.ranges[i] < scan.range_min || scan.ranges[i] > scan.range_max || isnan(scan.ranges[i])){
-       i++;
-       continue;
-    }
-
-    //get the x and y components of the vector to the point
-    laser_point.point.x = cos(currentAngle) * scan.ranges[i];
-    laser_point.point.y = sin(currentAngle) * scan.ranges[i];
-    try{
-      //transform that point to the base_link frame
-      pListener->transformPoint("base_link", laser_point, base_point);
-    }
-    catch(tf::TransformException& ex){
-      i++;
-      continue;
-    }
-    
-    //calculate the distance to point
-    double dist = sqrt(pow(base_point.point.x,2.0)+pow(base_point.point.y,2.0));
-    //if it is the closest point in the scan record it
-    if(dist < closestDist){
-      closestDist = dist;
-    }
-    i++;
-  }
-  
-  //record the new closest point in the buffer of last 3 closest distances
-  closestDists[index] = closestDist;
-  if(index+1==2) index=0;
-  else index++;
-
-  //take the closest distance of the last 3 scans to determine speed with
-  closestDist = scan.range_max;
-  for(i = 0; i < 2; i++)
-    if(closestDists[i] < closestDist)
-      closestDist = closestDists[i];
-
-  //set the throttle value based on the distance
-  if(closestDist < MIN_SAFE_DIST)
-    forward_throttle_safety_factor_base = 0.0;
-  else if(closestDist > START_SAFETY_THROTTLE_DIST)
-    forward_throttle_safety_factor_base = 1.0;
-  else{
-    forward_throttle_safety_factor_base = (closestDist-MIN_SAFE_DIST)/(START_SAFETY_THROTTLE_DIST-MIN_SAFE_DIST);
-    if(forward_throttle_safety_factor_base < 0.05) forward_throttle_safety_factor_base = 0.05;
-  }
-  
-}
-
-void carl_joy_teleop::map_cback(const nav_msgs::OccupancyGrid::ConstPtr& map){
-  //captures the map for later use
-  savedMap = *map;
-}
-
-void carl_joy_teleop::amcl_pose_cback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& pose){
-  //get distances in occupancy grid units 
-  static double startTrottleDistMap = (START_SAFETY_THROTTLE_DIST+0.15)/savedMap.info.resolution;
-  static double minSafeDistMap = (MIN_SAFE_DIST+0.1)/savedMap.info.resolution;
-
-  //get the yaw angle of robot in radians 
-  double angle = tf::getYaw(pose->pose.pose.orientation);
-  double closestDist;
-
-  //round estimated position to nearest occupancy grid cell
-  int i = round(pose->pose.pose.position.y/savedMap.info.resolution);
-  int j = round(pose->pose.pose.position.x/savedMap.info.resolution);
-
-  closestDist = startTrottleDistMap;
-  reverse_throttle_safety_factor_base = 0.8;
-  //for each grid cell that is within startTrottleDistMap of (x,y)
-  for(int x = i - (int)round(startTrottleDistMap); x < i + (int)round(startTrottleDistMap); x++){
-    for(int y = j - (int)round(startTrottleDistMap); y < j + (int)round(startTrottleDistMap); y++){
-
-      //if current point is within the map and is an obstical 
-      if(x >= 0 && x < savedMap.info.height && y >= 0 && y < savedMap.info.width 
-         && savedMap.data[(x*savedMap.info.width) + y] != 0){
-
-        //get the angle and distance of the point in relation to the robot in the grid
-        double ptAngle = atan2(x-i,y-j);
-        double dist = sqrt(pow(x-i,2)+pow(y-j,2));
-
-        double angleDiff = angle-ptAngle;
-        while (angleDiff < -M_PI) angleDiff += (2*M_PI);
-        while (angleDiff > M_PI) angleDiff -= (2*M_PI);
-
-        //if the point is the clostes point thus far and it is behind the robot
-        if(dist<=closestDist && angleDiff>(3*M_PI/4)){//(ptAngle<angle-(3*M_PI/4) || ptAngle>angle+(3*M_PI/4))){
-          //set new closest distance
-          closestDist = dist;
-          //if the distance is below the min allowed stop the robot
-          if(closestDist < minSafeDistMap)
-            reverse_throttle_safety_factor_base = 0.0; 
-          //else if it is in the trottle zone, linearly decearse the allowed speed 
-          //in relation to the distance of the point
-          else if(closestDist <= startTrottleDistMap){
-            reverse_throttle_safety_factor_base = (closestDist-minSafeDistMap)/(startTrottleDistMap-minSafeDistMap);
-            if(reverse_throttle_safety_factor_base < 0.05) reverse_throttle_safety_factor_base = 0.05;
-          }
-        } 
-
-      }
-    }
-  }
-  
-  if(reverse_throttle_safety_factor_base > 0.8) reverse_throttle_safety_factor_base = 0.8;
 }
 
 void carl_joy_teleop::publish_velocity()
