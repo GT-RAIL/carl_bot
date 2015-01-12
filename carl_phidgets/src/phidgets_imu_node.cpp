d698909eec4e35ef4845b44bbdf1a4d2959f0f66
David Kent <davidkent@wpi.edu>
2015-01-12 14:31:00 -0500

    carl_phidgets initial commit

---
+++ carl_phidgets/src/phidgets_imu_node.cpp
@@ -0,0 +1,11 @@
+#include "carl_phidgets/imu_ros_i.h"
+
+int main(int argc, char **argv)
+{
+  ros::init (argc, argv, "PhidgetsImu");
+  ros::NodeHandle nh;
+  ros::NodeHandle nh_private("~");
+  phidgets::ImuRosI imu(nh, nh_private);
+  ros::spin();
+  return 0;
+}
