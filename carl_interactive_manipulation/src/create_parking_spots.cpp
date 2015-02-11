95a1d169542a3d0a6a9bff8ee4bbd28779b45e7d
Peter <mitranopeter@gmail.com>
2015-02-11 10:05:37 -0500

    conformed to conventions

--- carl_interactive_manipulation/src/create_parking_spots.cpp
+++ carl_interactive_manipulation/src/create_parking_spots.cpp
@@ -1,49 +1,52 @@
-#include <carl_interactive_manipulation/create_parking_spots.hpp>
+#include <carl_interactive_manipulation/create_parking_spots.h>
 
-/** creates clickable navigation goals infront of furniture*/
-int main(int argc, char** argv){
+/**
+\brief runs an interacive marker server creates clickable navigation goals infront of furniture
+*/
+int main(int argc, char** argv)
+{
   ros::init(argc,argv,"create_parking_spots");
-  ros::NodeHandle node;
-  ros::Rate rate(10.0);
 
-  ParkingSpots parkingSpots;
-  parkingSpots.createParkingSpots();
+  CreateParkingSpots parkingSpots;
   
-  return 0;
+  return EXIT_SUCCESS;
 }
 
-ParkingSpots::ParkingSpots() : client_("move_base",true),server_("parking_markers") {
-}
-
-void ParkingSpots::createParkingSpots(){
+CreateParkingSpots::CreateParkingSpots() : client_("move_base",true),server_("parking_markers")
+{
   ROS_INFO("waiting for server...");
   
   bool status = client_.waitForServer();
   if (!status){
     ROS_INFO("Couldn't connect in time...");
   }
-  else {
+  else
+  {
 
   ROS_INFO("connected to server");
 
   urdf::Model ilab;
 
   //load the urdf with all the furniture off the param server
-  if (!ilab.initParam("/ilab_description")){
+  if (!ilab.initParam("/ilab_description"))
+  {
     ROS_INFO("couldn't find /ilab_description on param server.");
   }
-  else {
+  else 
+  {
 
     std::map<std::string, boost::shared_ptr<urdf::Link> > links = ilab.links_;
     std::map<std::string, boost::shared_ptr<urdf::Link> >::iterator itr;
     
     //go through all links and filter out the ones that end in "nav_goal_link"
-    for(itr = links.begin(); itr != links.end(); itr++) {
+    for(itr = links.begin(); itr != links.end(); itr++) 
+    {
       std::string link_name = itr->first;
-      if (isNavGoal(link_name)){
+      if (isNavGoal(link_name))
+      {
         visualization_msgs::InteractiveMarker marker = createParkingSpot(link_name);
         ROS_INFO("creating marker %s",link_name.c_str());
-        server_.insert(marker,boost::bind(&ParkingSpots::onClick, this, _1));
+        server_.insert(marker,boost::bind(&CreateParkingSpots::onClick, this, _1));
       }
     }
 
@@ -56,18 +59,17 @@ void ParkingSpots::createParkingSpots(){
   }
 }
 
-/**when you release the mouse on a marker this gets called
-eventually this will set the location of that marker as a nav goal for Carl */
-void ParkingSpots::onClick(const visualization_msgs::InteractiveMarkerFeedbackConstPtr &f){
+void CreateParkingSpots::onClick(const visualization_msgs::InteractiveMarkerFeedbackConstPtr &f)
+{
   if (f->event_type == visualization_msgs::InteractiveMarkerFeedback::MOUSE_UP){
 
     //copy header and pose from marker to new action goal
     //rotate 90 deg around z axis
     geometry_msgs::Pose new_pose = f->pose;
-    new_pose.orientation.x=0;
-    new_pose.orientation.y=0;
-    new_pose.orientation.z=1;
-    new_pose.orientation.w=1;
+    new_pose.orientation.x = 0;
+    new_pose.orientation.y = 0;
+    new_pose.orientation.z = 1;
+    new_pose.orientation.w = 1;
 
     //create pose
     geometry_msgs::PoseStamped target_pose;
@@ -83,28 +85,29 @@ void ParkingSpots::onClick(const visualization_msgs::InteractiveMarkerFeedbackCo
 
     bool finished_before_timeout = client_.waitForResult(ros::Duration(30.0));
 
-    if (finished_before_timeout){
+    if (finished_before_timeout)
+    {
       ROS_INFO("finished successfully!");
     }
-    else {
+    else 
+    {
       ROS_INFO("timed out! goal not reached in 30 seconds");
     }
   }   
 }
 
-/**returns true only if the string ends in "nav_goal_link"*/
-bool ParkingSpots::isNavGoal(std::string link_name){
+bool CreateParkingSpots::isNavGoal(std::string link_name)
+{
   std::string search_param = "nav_goal_link";
   return link_name.find(search_param,link_name.length()-search_param.length()) != std::string::npos;
 }
 
-/**creates a clickable marker at the origin of the given frame id
-this frame id is a string of the name of a link*/
-visualization_msgs::InteractiveMarker ParkingSpots::createParkingSpot(std::string frame_id){
+visualization_msgs::InteractiveMarker CreateParkingSpots::createParkingSpot(std::string frame_id)
+{
   visualization_msgs::InteractiveMarker int_marker;
   int_marker.header.frame_id = frame_id;
   int_marker.scale = 1;
-  int_marker.name = frame_id+"_parking_spot";
+  int_marker.name = frame_id + "_parking_spot";
   
   visualization_msgs::InteractiveMarkerControl control;
   control.interaction_mode = visualization_msgs::InteractiveMarkerControl::BUTTON;
@@ -112,13 +115,13 @@ visualization_msgs::InteractiveMarker ParkingSpots::createParkingSpot(std::strin
   
   visualization_msgs::Marker box;
   box.type = visualization_msgs::Marker::CUBE;
-  box.scale.x=0.15;
-  box.scale.y=0.15;
-  box.scale.z=0.05;
-  box.color.r=0;
-  box.color.g=0.5;
-  box.color.b=0.25;
-  box.color.a=1;
+  box.scale.x = 0.15;
+  box.scale.y = 0.15;
+  box.scale.z = 0.05;
+  box.color.r = 0;
+  box.color.g = 0.5;
+  box.color.b = 0.25;
+  box.color.a = 1;
  
   control.markers.push_back(box);
   control.always_visible = true;
